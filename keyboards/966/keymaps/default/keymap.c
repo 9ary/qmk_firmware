636edeaf4ec7bdc1fa4c2ed85f13cc78974f0850
novenary <streetwalkermc@gmail.com>
2022-07-25 10:26:07 +0300

    Fix "menu" keycode

--- keyboards/966/keymaps/default/keymap.c
+++ keyboards/966/keymaps/default/keymap.c
@@ -2,7 +2,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_default(
-        KC_MENU, KC_GRAVE, KC_1, KC_2, KC_3, KC_4, KC_5, KC_LEFT_GUI,
+        KC_APPLICATION, KC_GRAVE, KC_1, KC_2, KC_3, KC_4, KC_5, KC_LEFT_GUI,
         KC_RIGHT_GUI, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINUS, KC_EQUAL,
 
         KC_BACKSLASH, KC_SLASH, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_DELETE, KC_DELETE,
