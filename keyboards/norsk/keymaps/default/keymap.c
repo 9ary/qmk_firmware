e379cf62ec6b28c6d87717bc55be64f7da09a6f4
novenary <streetwalkermc@gmail.com>
2022-10-12 12:40:39 +0300

    Unsplit bracket keys

--- keyboards/norsk/keymaps/default/keymap.c
+++ keyboards/norsk/keymaps/default/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_SLSH, KC_Q,    KC_W,    KC_E,    KC_R,           KC_T,    KC_DEL,
         KC_LSPO, KC_A,    KC_S,    KC_D,    KC_F,           KC_G,    KC_TAB,
         KC_LCPO, KC_Z,    KC_X,    KC_C,    KC_V,           KC_B,    KC_LALT,
-        KC_LAPO, KC_HOME, KC_PGUP, KC_END,  TD(DANCE_LGUI), KC_SPC,  KC_ENT,
+        KC_LGUI, KC_HOME, KC_PGUP, KC_END,  TD(DANCE_LGUI), KC_SPC,  KC_ENT,
                           KC_PGDN,
 
         // Right hand
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX,         KC_Y,    KC_U,      KC_I,    KC_O,    KC_P,    KC_EQL,
         MO(LAYER_MEDIA), KC_H,    KC_J,      KC_K,    KC_L,    KC_SCLN, KC_RSPC,
         KC_RALT,         KC_N,    KC_M,      KC_COMM, KC_DOT,  KC_QUOT, KC_RCPC,
-        KC_BSPC,         KC_RSFT, KC_SFTENT, KC_LEFT, KC_UP,   KC_RGHT, KC_RAPC,
+        KC_BSPC,         KC_RSFT, KC_SFTENT, KC_LEFT, KC_UP,   KC_RGHT, KC_RGUI,
                                                       KC_DOWN
     ),
 
