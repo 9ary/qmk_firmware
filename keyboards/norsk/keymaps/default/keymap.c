cc58f0b34d331ab0e8e5ac807aa474e0e472684d
novenary <streetwalkermc@gmail.com>
2022-11-19 13:32:07 +0200

    Enable one-shot shift

--- keyboards/norsk/keymaps/default/keymap.c
+++ keyboards/norsk/keymaps/default/keymap.c
@@ -48,11 +48,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                           KC_PGDN,
 
         // Right hand
-        KC_INS,          KC_6,    KC_7,      KC_8,    KC_9,    KC_0,    KC_MINS,
-        XXXXXXX,         KC_Y,    KC_U,      KC_I,    KC_O,    KC_P,    KC_EQL,
-        MO(LAYER_MEDIA), KC_H,    KC_J,      KC_K,    KC_L,    KC_SCLN, KC_RSPC,
-        KC_RALT,         KC_N,    KC_M,      KC_COMM, KC_DOT,  KC_QUOT, KC_RCPC,
-        KC_BSPC,         KC_RSFT, KC_SFTENT, KC_LEFT, KC_UP,   KC_RGHT, KC_RGUI,
+        KC_INS,          KC_6,          KC_7,      KC_8,    KC_9,    KC_0,    KC_MINS,
+        XXXXXXX,         KC_Y,          KC_U,      KC_I,    KC_O,    KC_P,    KC_EQL,
+        MO(LAYER_MEDIA), KC_H,          KC_J,      KC_K,    KC_L,    KC_SCLN, KC_RSPC,
+        KC_RALT,         KC_N,          KC_M,      KC_COMM, KC_DOT,  KC_QUOT, KC_RCPC,
+        KC_BSPC,         OSM(MOD_RSFT), KC_SFTENT, KC_LEFT, KC_UP,   KC_RGHT, KC_RGUI,
                                                       KC_DOWN
     ),
 
